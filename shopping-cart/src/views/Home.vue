cb18dc9e7923bc52d52ee6b32cb517c28ace97f3
Lars Lemmens <lars.lemmens@student.ucll.be>
2022-04-19 15:15:47 +0200

    home.vue

---
+++ shopping-cart/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>PRODUCTEN</h1>
+    <h2>{{products}}</h2>
+</template>
+	
+<script>
+// @ is an alias to /src
+	
+export default {
+  name: 'Home',
+  computed: {
+    products() {
+      return this.$store.state.products;
+    }
+  },
+  components: {
+    
+  }
+}
+</script>
